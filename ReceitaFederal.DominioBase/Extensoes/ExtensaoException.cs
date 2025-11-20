6001257c804ad5e6ee1fe67147c0bf5b69b3a7bb
João do Reis Lemos Fontana <joaorlfontana@gmail.com>
2025-11-20 15:26:42 -0300

    Criação do projeto ReceitaFederal.DominioBase.

---
+++ ReceitaFederal.DominioBase/Extensoes/ExtensaoException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace ReceitaFederal.DominioBase.Extensoes
+{
+    public static class ExtensaoException
+    {
+
+        public static IEnumerable<string> ObterMensagensException(this Exception exception)
+        {
+            do
+            {
+                yield return exception.Message;
+                exception = exception.InnerException;
+            }
+            while (exception is not null);
+        }
+
+
+        public static string ObterMensagensExceptionConcatenadas(this Exception exception, string separador = " | ") =>
+            string.Join(separador, exception.ObterMensagensException());
+
+
+        public static string ObterMensagensExceptionConcatenadas(this JsonException exception, string separador = " | ")
+        {
+            List<string> mensagens = [$"Linha [{exception.LineNumber}]", $"Caminho [{exception.Path}]"];
+            mensagens.AddRange(exception.ObterMensagensException());
+            return string.Join(separador, exception.ObterMensagensException());
+        }
+    }
+}
---
+++ ReceitaFederal.DominioBase/FerramentasJson/ObjetoSerializador.cs
@@ -0,0 +1,38 @@
+﻿using ReceitaFederal.DominioBase.Extensoes;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ReceitaFederal.DominioBase.FerramentasJson
+{
+    public static class ObjetoSerializador<T>
+    {
+        private readonly static JsonSerializerOptions _jsonSerializarOptions = new()
+        {
+            WriteIndented = true,
+            Converters =
+            {
+                new JsonStringEnumConverter()
+            }
+        };
+
+        public static string Serializar(T objeto) =>
+            JsonSerializer.Serialize(objeto, _jsonSerializarOptions);
+
+        public static T Desserializar(string json)
+        {
+            try
+            {
+                return JsonSerializer.Deserialize<T>(json);
+            }
+            catch (JsonException jsonException)
+            {
+                throw new Exception(jsonException.ObterMensagensExceptionConcatenadas());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
